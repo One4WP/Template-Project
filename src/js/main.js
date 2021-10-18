3c450864c1c175a13087fa66b910805f442191d1
Weblis <Weblis>
2021-10-18 10:54:45 +0300

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  if (window.location.pathname == '/') {
+
+  }
+  else if (window.location.pathname == '/about.html') {
+
+  }
+  else {
+
+  }
+
+})
